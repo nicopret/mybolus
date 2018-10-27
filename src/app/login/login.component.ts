f1886ce2481c5fc1f0d98ff146ede8493fb5ea18
Nico Pretorius <nico.pretorius@pbtgroup.co.za>
2018-10-27 21:13:52 +0200

    Styling login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     moduleId: module.id,
     selector: "app-login",
+    styleUrls: [ "login.component.css" ],
     templateUrl: "login.component.html"
 })
 export class LoginComponent {}
